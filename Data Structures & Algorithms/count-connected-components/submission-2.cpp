88812fa805a6152b2531dea4ec4a19d8408fbf89
Raghav50w <66841292+Raghav50w@users.noreply.github.com>
2026-07-11 00:31:03 +0530

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj(n);
+        vector<bool> visit(n, false);
+        for(auto& edge: edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(!visit[i]) {
+                bfs(i, adj, visit);
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
+    void bfs(int i, unordered_map<int, vector<int>>& adj, vector<bool>& visit) {
+        queue<int> q;
+        q.push(i);
+
+        while(!q.empty()) {
+            int curr = q.front();
+            visit[curr] = true;
+            q.pop();
+
+            for(int nei: adj[curr]) {
+                if(!visit[nei]) {
+                    q.push(nei);
+                }
+            }
+        }
+    }
+};
